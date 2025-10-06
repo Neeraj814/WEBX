0904ecff0989f2de4a2096792bdf5ecd6f51a6b5
Neeraj Kumar Bhoi <158065489+Neeraj814@users.noreply.github.com>
2025-10-06 21:50:25 +0530

    first-commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value.toString().length >= 10;
+            },
+            message: "Phone number must be at least 10 digits long"
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['student', 'recruiter'],
+        required: true
+    },
+    profile: {
+        bio: { type: String },
+        skills: [{ type: String }],
+        resume: { type: String }, // URL to resume file
+        resumeOriginalName: { type: String },
+        company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+        profilePhoto: {
+            type: String,
+            default: ""
+        }
+    },
+}, { timestamps: true });
+export const User = mongoose.model('User', userSchema);
